7b64db4d695e1203a71ccdc69dea4660ce3754ef
Hjalte Albergson <123952556+HAlbergson@users.noreply.github.com>
2024-09-04 09:46:09 +0200

    inputfelt, og button tilføjet

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -1,14 +1,25 @@
 // screens/AddItemScreen.js
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Button, TextInput } from "react-native-web";
 
 export default function AddItemScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Add a new item to your list</Text>
-    
-    </View>
-  );
+  const [text, setText] = React.useState("");
+  const [list, setList] = React.useState([]);
+
+  const addItem = () => {
+    if (text) {
+      setList([...list, text]);
+      setText("");
+    }
+  };
+
+  return;
+  <View style={styles.container}>
+    <Text>Add a new item to your list</Text>
+    <TextInput placeholder="Enter item name" value={text} onChangeText={setText} />
+    <Button title="Add Item" onPress={addItem} />
+  </View>;
 }
 
 const styles = StyleSheet.create({
